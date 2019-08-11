5129b3a3c64a826bf79ded1fc40c4adb91b45c46
Ray Power <39620873+raypower139@users.noreply.github.com>
2019-08-11 19:06:20 +0100

    Header Component Added

--- stories/index.js
+++ stories/index.js
@@ -1,3 +1,8 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import Header from '../src/components/header/'
+
+storiesOf("Movie List App/Header", module).add("default", () => (
+    <Header noMovies={10} />
+));
